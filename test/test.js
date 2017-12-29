8b4ef2e1285bcf650fe9435b40df7a5b549b368c
huymaidinh <maidinhhuy.94@gmail.com>
2017-12-29 15:10:40 +0700

    add test file

---
+++ test/test.js
@@ -0,0 +1,18 @@
+const supertest = require("supertest");
+const should = require("should");
+
+var server = supertest.agent("http://localhost:3000");
+
+describe('Sample unit test', function () {
+    it('should return home page', function (done) {
+        server
+            .get("/")
+            .expect("Content-type", /text/)
+            .expect(200)
+            .end(function (err, res) {
+                res.status.should.equal(200);
+                done();
+            });
+    });
+
+});
